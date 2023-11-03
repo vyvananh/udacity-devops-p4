11bbc8fefbb7885dd3748429080313fcf463d7be
vyanh <vyvananh99@gmail.com>
2023-11-03 13:21:24 +0900

    first commit

---
+++ starter/frontend/.eslintrc.js
@@ -0,0 +1,39 @@
+const prettier = require('prettier');
+
+module.exports = {
+  ignorePatterns: ['build/', 'dist/', 'node_modules/'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  plugins: ['react', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        jsxSingleQuote: false,
+        printWidth: 120,
+        parser: 'babel-ts',
+      },
+    ],
+    // Customize your rules here
+    'react/prop-types': process.env.FAIL_LINT ? 2 : 0,
+    'react/jsx-uses-vars': 'warn',
+  },
+};
